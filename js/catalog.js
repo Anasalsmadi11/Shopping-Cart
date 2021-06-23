0e31b8f6cf5bd2ff2dd3eaff3e2c57f801026693
moayadalhaj <moayad.alhaj21@gmail.com>
2021-06-23 20:59:51 +0300

    Create a cart for the products

--- js/catalog.js
+++ js/catalog.js
@@ -3,7 +3,7 @@
 'use strict';
 
 // Set up an empty cart for use on this page.
-const cart = new Cart([]);
+let cart = new Cart([]);
 
 // On screen load, we call this method to put all of the busmall options
 // (the things in the Product.allProducts array) into the drop down list.
@@ -21,32 +21,41 @@ function populateForm() {
 // When someone submits the form, we need to add the selected item to the cart
 // object, save the whole thing back to local storage and update the screen
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
-function handleSubmit(event) {
 
+function handleSubmit(event) {
   // TODO: Prevent the page from reloading
   event.preventDefault();
   // Do all the things ...
-  addSelectedItemToCart();
+  addSelectedItemToCart(event.target.items.value, event.target.quantity.value);
   cart.saveToLocalStorage();
   updateCounter();
   updateCartPreview();
-
 }
 
 // TODO: Add the selected item and quantity to the cart
-function addSelectedItemToCart() {
+function addSelectedItemToCart(product, quantity) {
   // TODO: suss out the item picked from the select list
   // TODO: get the quantity
   // TODO: using those, add one item to the Cart
+  cart.addItem(product, quantity);
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() { }
+function updateCounter() {
+  let listItems = document.getElementById('itemCount');
+  listItems.textContent = cart.items.length;
+}
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
 function updateCartPreview() {
-  // TODO: Get the item and quantity from the form
+
   // TODO: Add a new element to the cartContents div with that information
+  let cartContents = document.getElementById('cartContents');
+  let pElement = document.createElement('p')
+  for (let i in cart.items) {
+    pElement.textContent = ` You have added a ${cart.items[i].quantity}  of ${cart.items[i].product}`;
+    cartContents.appendChild(pElement);
+  }
 }
 
 // Set up the "submit" event listener on the form.
