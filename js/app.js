650e8e14ee1cfc5ee1328d9b2517b5b0f927c1f1
balqessAlfasatlah <BalqeesAlfasatlah@yahoo.com>
2021-06-23 16:09:01 +0300

    finish part2

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@
 const Cart = function(items) {
   // this.items is an array of CartItem instances.
   this.items = items;
+  
 };
+
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
